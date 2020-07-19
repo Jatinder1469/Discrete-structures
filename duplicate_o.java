e78a715120671a877562e4f6010a1470249edfc4
Jatinder1469 <52443543+Jatinder1469@users.noreply.github.com>
2020-07-19 22:52:52 +0530

    Add files via upload

---
+++ duplicate_o.java
@@ -0,0 +1,19 @@
+package ds;
+import java.util.*;
+public class duplicate_o {
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+       int a[]= {0,1,0,-1,-4,-1,6,7,8,8};
+       Set<Integer> set=new HashSet<Integer>();
+       for(int i=0;i<a.length;i++)
+       {
+    	   if(set.add(a[i])==false)
+    	   {
+    		   System.out.println(a[i]+" ");
+    	   }
+       }
+	}
+	}
+
+
