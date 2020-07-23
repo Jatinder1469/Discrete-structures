b7b3bfabb5fa2fb76d5373ed33c764f6326a2a05
Jatinder1469 <52443543+Jatinder1469@users.noreply.github.com>
2020-07-23 09:50:34 +0530

    Update duplicate_o.java

--- duplicate_o.java
+++ duplicate_o.java
@@ -4,7 +4,7 @@ public class duplicate_o {
 	public static void main(String args[])
 	{
 		Scanner s=new Scanner(System.in);
-       int a[]= {0,1,0,-1,0,-1,=2,=4,5,8};
+       int a[]= {0,1,0,-1,0,-1,2,-4,5,8};
        Set<Integer> set=new HashSet<Integer>();
        for(int i=0;i<a.length;i++)
        {
